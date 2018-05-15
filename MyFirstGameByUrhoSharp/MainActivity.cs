037683aae67aaf50661c71527213e17115f6ce60
Joe Lv <joel@wicresoft.com>
2018-05-15 14:58:14 +0800

    updat

--- MyFirstGameByUrhoSharp/MainActivity.cs
+++ MyFirstGameByUrhoSharp/MainActivity.cs
@@ -9,16 +9,18 @@ using System.Linq;
 using Android.Runtime;
 using Android.Content.PM;
 using Android.Util;
+using System.Collections.Generic;
+using System;
 
 namespace MyFirstGameByUrhoSharp
 {
-    [Activity(Label = "MyFirstGameByUrhoSharp", MainLauncher = true, Icon = "@mipmap/icon",ScreenOrientation = ScreenOrientation.Portrait)]
+    [Activity(Label = "MyFirstGameByUrhoSharp", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity,ISensorEventListener
     {
         UrhoSurfacePlaceholder surface;
         MyApp app;
         private SensorManager mSensorManager;
-        private Sensor mRotationSensor;
+        private Sensor mAccSensor,mMagneticSensor, mGeomagneticSensor;
         protected override async void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -30,8 +32,12 @@ namespace MyFirstGameByUrhoSharp
 
 
             mSensorManager = (SensorManager)GetSystemService(Activity.SensorService);
-            mRotationSensor = mSensorManager.GetDefaultSensor(SensorType.RotationVector);
-            mSensorManager.RegisterListener(this, mRotationSensor, SensorDelay.Game);
+            mAccSensor = mSensorManager.GetDefaultSensor(SensorType.Accelerometer);
+            mMagneticSensor = mSensorManager.GetDefaultSensor(SensorType.MagneticField);
+            mGeomagneticSensor = mSensorManager.GetDefaultSensor(SensorType.GeomagneticRotationVector);
+            mSensorManager.RegisterListener(this, mAccSensor, SensorDelay.Game);
+            mSensorManager.RegisterListener(this, mMagneticSensor, SensorDelay.Game);
+            mSensorManager.RegisterListener(this, mGeomagneticSensor, SensorDelay.Game);
         }
 
         protected override void OnResume()
@@ -55,6 +61,7 @@ namespace MyFirstGameByUrhoSharp
         protected override void OnDestroy()
         {
             UrhoSurface.OnDestroy();
+            mSensorManager.UnregisterListener(this);
             base.OnDestroy();
         }
 
@@ -77,10 +84,10 @@ namespace MyFirstGameByUrhoSharp
             base.OnWindowFocusChanged(hasFocus);
         }
 
-
+        float[] data_acc = new float[3], data_mag = new float[3];
         public void OnSensorChanged(SensorEvent e)
         {
-            if (e.Sensor == mRotationSensor)
+           /* if (e.Sensor.Type == SensorType.GeomagneticRotationVector)
             {
                 var rm = new float[9];
                 SensorManager.GetRotationMatrixFromVector(rm, e.Values.ToArray());
@@ -94,6 +101,39 @@ namespace MyFirstGameByUrhoSharp
                 app.cameraNode.Rotation = new Urho.Quaternion(app.pitch, app.yaw, 0);
 
             }
+            */
+            SensorType type = e.Sensor.Type;
+            if (type == SensorType.Accelerometer) {
+                   data_acc = e.Values.ToArray();
+            }
+            if (type == SensorType.MagneticField) {
+                   data_mag = e.Values.ToArray();
+            }
+            
+            if (data_acc!=null &&data_mag!=null) {
+                float[] R = new float[9];
+                float[] I = new float[9];
+
+                bool success = SensorManager.GetRotationMatrix(R, I, data_acc, data_mag);
+                if (success)
+                {
+                    float[] orientation = new float[3];
+                    SensorManager.GetOrientation(R, orientation);
+                    // orientation contains: azimut, pitch and roll
+                    app.pitch = orientation[1];
+                    app.yaw = orientation[2];
+                    //app.pitch = (Urho.MathHelper.RadiansToDegrees(orientation[1]) + 360) % 360;      // map [-Pi...+Pi] to [0...360]
+                    //app.yaw = (Urho.MathHelper.RadiansToDegrees(orientation[2]) + 360) % 360;
+                    Log.Error("pitch=", orientation[1] + "");
+                    Log.Error("yaw=", orientation[2] + "");
+                    app.cameraNode.Rotation = new Urho.Quaternion(app.pitch, app.yaw, 0);
+                }
+                
+
+              
+            }
+           
+            
         }
 
         public void OnAccuracyChanged(Sensor sensor, [GeneratedEnum] SensorStatus accuracy)
